89a34bc2af1bf2be5fec9525d3b0183395dd91db
l2danted <dante_domizioli@hotmail.com>
2021-04-28 15:23:39 -0300

    Hice ej 3 y 4

--- clase1.py
+++ clase1.py
@@ -21,9 +21,35 @@ def dos():
     except ValueError:
         print("El número debe ser float (excepción).")
 
+# 3
+def tres():
+    contador = 0
+    lista = []
+    while contador <= 4:
+        numero_entero = int(input("Coloque un número entero: "))
+        lista.append(numero_entero)
+        contador += 1
+    
+    for y in lista:
+        print(y)
+
+#4
+def cuatro():
+    lista1 = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
+    for y in lista1:
+        if y > 150:
+            break
+        else:
+            if y % 5 == 0:
+                print(y)
+
 # Main
 numero_de_ejercicio = int(input("Seleccione el ejercicio: "))
 if numero_de_ejercicio == 1:
     uno()
 elif numero_de_ejercicio == 2:
     dos()
+elif numero_de_ejercicio == 3:
+    tres()
+elif numero_de_ejercicio == 4:
+    cuatro()
